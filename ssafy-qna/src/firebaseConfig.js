817862627a1739ce168369725647610c22a736e5
김성우 <rlatjddn23@gmail.com>
2019-10-16 16:23:52 +0900

    seperate firebase Config

---
+++ ssafy-qna/src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const snippet = {
+  apiKey: "AIzaSyCRq_nISqiR-itSoaVPS8KyrxiEdalKddo",
+  authDomain: "ssafyfinal.firebaseapp.com",
+  databaseURL: "https://ssafyfinal.firebaseio.com",
+  projectId: "ssafyfinal",
+  storageBucket: "ssafyfinal.appspot.com",
+  messagingSenderId: "752151326570",
+  appId: "1:752151326570:web:d4d773f1ea0237a4273f13",
+  measurementId: "G-ZE1GDYK77V"
+};
