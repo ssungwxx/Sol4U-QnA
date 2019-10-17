a06df2097c4eba7d4553d32c87fcb54ffd1b7196
신승민 <sammiffy77@naver.com>
2019-10-17 16:24:16 +0900

    Add base form of signup

---
+++ ssafy-qna/src/components/SignUp.vue
@@ -0,0 +1,92 @@
+<template>
+  <v-row>
+    <v-btn color="primary" dark @click.stop="dialog = true">Sign Up</v-btn>
+
+    <v-dialog v-model="dialog" max-width="500">
+      <v-card>
+        <v-card-title>
+          <span class="headline">Sign Up</span>
+        </v-card-title>
+        <v-form ref="form" v-model="valid" lazy-validation>
+          <v-card-text>
+            <v-container>
+              <v-text-field
+                v-model="id"
+                :counter="15"
+                :rules="idRules"
+                label="ID*"
+                hint="Password must be at least 8 characters"
+                required
+              ></v-text-field>
+              <v-text-field
+                v-model="pw"
+                :append-icon="show ? 'visibility' : 'visibility_off'"
+                :type="show ? 'text':'password'"
+                :counter="15"
+                :rules="pwRules"
+                label="Password*"
+                hint="Password must be at least 8 characters"
+                @click:append="show = !show"
+                required
+              ></v-text-field>
+            </v-container>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+
+            <v-btn color="green darken-1" text @click="close">Close</v-btn>
+            <v-btn color="green darken-1" text @click="reset">Reset</v-btn>
+            <v-btn color="green darken-1" text @click="submit">Submit</v-btn>
+          </v-card-actions>
+        </v-form>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false,
+      valid: true,
+      dialog: false,
+      id: "",
+      idRules: [
+        v => !!v || "ID is required",
+        v =>
+          (v && v.length <= 15 && v.length >= 6) ||
+          "ID must be at least 6 and less than 15 characters"
+      ],
+      pw: "",
+      pwRules: [
+        v => !!v || "Password is required",
+        v =>
+          (v && v.length <= 15 && v.length >= 8) ||
+          "Password must be at least 8 and less than 15 characters"
+      ]
+    };
+  },
+  methods: {
+    submit() {
+      if (this.$refs.form.validate()) {
+        alert("Sign up success!");
+        this.reset();
+        this.dialog = false;
+      }
+    },
+    reset() {
+      this.$refs.form.reset();
+    },
+    close() {
+      this.dialog = false;
+      this.reset();
+    }
+  },
+  watch: {
+    dialog: function() {
+      this.reset();
+    }
+  }
+};
+</script>
--- ssafy-qna/src/views/JoinPage.vue
+++ ssafy-qna/src/views/JoinPage.vue
@@ -1,54 +1,55 @@
 <template>
-    <v-app>
-        <div class = "backgroundImage">
-            <div class = "link_space">
-                <div class = "input_link">
-                    <v-text-field
-                    v-model="code"
-                    label="Code">
-                    </v-text-field>
-                </div>
-            </div>
+  <v-app>
+    <div class="backgroundImage">
+      <div class="link_space">
+        <div class="input_link">
+          <v-text-field v-model="code" label="Code"></v-text-field>
+          <SignUp></SignUp>
         </div>
-    </v-app>
+      </div>
+    </div>
+  </v-app>
 </template>
 
 <script>
-export default ({
+import SignUp from "../components/SignUp";
+export default {
+  components: {
+    SignUp
+  },
   data: () => ({
-    code: ''
+    code: ""
   })
-})
+};
 </script>
 
 <style>
-  .backgroundImage{
-    border:0;
-    padding:0;
-    background-image:url("../assets/kong.jpg");
-    min-height:100%;
-    background-position: center;
-    background-size: cover;
-    opacity: 0.85;
-  }
+.backgroundImage {
+  border: 0;
+  padding: 0;
+  background-image: url("../assets/kong.jpg");
+  min-height: 100%;
+  background-position: center;
+  background-size: cover;
+  opacity: 0.85;
+}
 
-  .link_space{
-    margin-top:250px;
-    height: 80px;
-    text-align:center;
-  }
+.link_space {
+  margin-top: 250px;
+  height: 80px;
+  text-align: center;
+}
 
-  .input_link{
-    text-align:center;
-    width:400px;
-    height:80px;
-    background-color:white;
-    border-radius: 10px;
-    opacity:0.6;
-    display: inline-block;
-    padding-left:30px;
-    padding-right:30px;
-    padding-top:10px;
-  }
-
+.input_link {
+  text-align: center;
+  width: 400px;
+  height: 80px;
+  background-color: white;
+  border-radius: 10px;
+  opacity: 0.6;
+  display: inline-block;
+  padding-left: 30px;
+  padding-right: 30px;
+  padding-top: 10px;
+}
 </style>
