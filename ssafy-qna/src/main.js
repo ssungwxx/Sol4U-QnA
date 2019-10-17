0c74ccdf9903324cd0cac85a61ea583124d1d175
changgyu <sa3590@naver.com>
2019-10-17 11:56:41 +0900

    add JoinPage

--- ssafy-qna/src/main.js
+++ ssafy-qna/src/main.js
@@ -1,5 +1,4 @@
 import Vue from 'vue'
-import './plugins/axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -10,19 +9,20 @@ import firebase from 'firebase'
 Vue.config.productionTip = false
 
 let config = {
-  apiKey: "AAAArx-2b2o:APA91bF9-deVNiDvvlT2XYKZqiXNGYmzqHD6zs45AM3st8ZSbBiQu83zp24zefOe_d8Ng2RkOBmDmCR3jVegbbSQq3VjFLuTmiokfv6kQlP9U_1H7RfGQvUG9Tp308HSAeX9-DVGsFTU",
-  authDomain: "https://ssafyfinal.firebaseapp.com/",
-  databaseURL: "https://ssafyfinal.firebaseio.com",
-  projectId: "ssafyfinal",
-  storageBucket: "ssafyfinal.appspot.com",
-  messagingSenderId: "752151326570"
-};
+  apiKey: 'AAAArx-2b2o:APA91bF9-deVNiDvvlT2XYKZqiXNGYmzqHD6zs45AM3st8ZSbBiQu83zp24zefOe_d8Ng2RkOBmDmCR3jVegbbSQq3VjFLuTmiokfv6kQlP9U_1H7RfGQvUG9Tp308HSAeX9-DVGsFTU',
+  authDomain: 'https://ssafyfinal.firebaseapp.com/',
+  databaseURL: 'https://ssafyfinal.firebaseio.com',
+  projectId: 'ssafyfinal',
+  storageBucket: 'ssafyfinal.appspot.com',
+  messagingSenderId: '752151326570'
+}
 
-firebase.initializeApp(config);
+firebase.initializeApp(config)
 
 new Vue({
   router,
   store,
   vuetify,
+  axios,
   render: h => h(App)
 }).$mount('#app')
---
+++ ssafy-qna/src/views/JoinPage.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-app>
+  <div class = "backgroundImage">
+    <div class = "link_space">
+      <div class = "input_link">
+        
+      </div>
+    </div>
+  </div>
+  </v-app>
+</template>
+
+<script>
+export default ({
+  data: () => ({
+    code: ''
+  })
+})
+</script>
+
+<style>
+  .backgroundImage{
+    border:0;
+    padding:0;
+    background-image:url("../assets/kong.jpg");
+    min-height:100%;
+    background-position: center;
+    background-size: cover;
+    opacity: 0.85;
+  }
+
+  .link_space{
+    margin-top:250px;
+    height: 80px;
+    text-align:center;
+  }
+
+  .input_link{
+    width:400px;
+    height:80px;
+    background-color:white;
+    border-radius: 10px;
+    opacity:0.6;
+    display: inline-block;
+  }
+</style>
