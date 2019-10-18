67fb845f5502f2e44849f7dcf06674fc58e07f65
jpark <heyheygo99@naver.com>
2019-10-18 09:49:57 +0900

    Update Home.vue
    router  수정

--- ssafy-qna/src/views/Home.vue
+++ ssafy-qna/src/views/Home.vue
@@ -11,7 +11,7 @@
       <v-row id="rowField">
         <v-col sm="3" cols="12">
           <v-text-field v-model="code" label="Code Number" id="inputCode"></v-text-field>
-          <router-link :to=""/qna/"+code">
+          <router-link :to="'/qna/'+code">
             <!-- 여기에 vuex에 Guest아이디로 넘겨주는 기능 추가하면됨 -->
             <v-btn class="ma-2" style="width:180px" outlined color="indigo">Guest</v-btn>
           </router-link>
