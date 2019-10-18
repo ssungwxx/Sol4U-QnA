a801a1ea7457b3def37c2322c4ef3f9bd8e5e658
jpark <heyheygo99@naver.com>
2019-10-18 10:48:53 +0900

    edit sources
    자동 세팅 적용

--- ssafy-qna/src/App.vue
+++ ssafy-qna/src/App.vue
@@ -1,26 +1,25 @@
 <template>
   <v-app>
-      <router-view/>
+    <router-view />
   </v-app>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  name: "App",
   data: () => ({
     //
   })
-}
+};
 
-var agent = navigator.userAgent.toLowerCase()
-if (agent.indexOf('chrome') === -1) {
-  alert('해당 홈페이지는 Chrome에 최적화 되어 있습니다.')
+var agent = navigator.userAgent.toLowerCase();
+if (agent.indexOf("chrome") === -1) {
+  alert("해당 홈페이지는 Chrome에 최적화 되어 있습니다.");
 }
-
 </script>
 
 <style>
 body::-webkit-scrollbar {
-    display: none;
+  display: none;
 }
 </style>
