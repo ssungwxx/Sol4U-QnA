586f8a2f0da0213a70ff757f4229ffed6d7298f7
신승민 <sammiffy77@naver.com>
2019-10-25 15:04:48 +0900

    Add isLogin to buttons

--- ssafy-qna/src/views/Home.vue
+++ ssafy-qna/src/views/Home.vue
@@ -14,6 +14,7 @@
           <v-text-field v-model="code" label="Code Number" id="inputCode"></v-text-field>
           <!-- 여기에 vuex에 Guest아이디로 넘겨주는 기능 추가하면됨 -->
           <v-btn
+            v-if="!getIsLogin"
             class="ma-2 btnHome"
             style="width:200px"
             outlined
@@ -25,6 +26,7 @@
 
           <router-link :to="'/channel/create'">
             <v-btn
+              v-if="getIsLogin"
               class="ma-2 btnHome"
               style="width:200px"
               outlined
@@ -33,6 +35,7 @@
             >Create</v-btn>
           </router-link>
           <v-btn
+            v-if="!getIsLogin"
             class="ma-2 btnHome"
             style="width:200px"
             outlined
@@ -42,7 +45,14 @@
             <v-icon color="red">fa-google</v-icon>&nbsp;Login with Google
           </v-btn>
 
-          <v-btn class="ma-2 btnHome" style="width:200px" outlined color="red" @click="logout">
+          <v-btn
+            v-if="getIsLogin"
+            class="ma-2 btnHome"
+            style="width:200px"
+            outlined
+            color="red"
+            @click="logout"
+          >
             <v-icon color="red">fa-google</v-icon>&nbsp;Logout
           </v-btn>
         </v-col>
