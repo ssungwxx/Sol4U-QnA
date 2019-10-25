144109effa717082223fd95770fa12b5265bb6c3
changgyu <sa3590@naver.com>
2019-10-25 14:46:47 +0900

    update home page

--- ssafy-qna/src/views/Home.vue
+++ ssafy-qna/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div id="containerHome">
     <!-- <ImageBanner imgSrc="https://picsum.photos/1600/900" id="imgBanner"> -->
     <ImageBanner id="imgBanner">
-      <div style="line-height:1.2em; color: white;" slot="text" class="font-weight-bold fontBanner">
+      <div id="bannerText" style="line-height:1.2em; color: white;" slot="text" class="font-weight-bold fontBanner">
         Sol4U
         <br />SSAFY QnA Service
       </div>
@@ -10,11 +10,11 @@
 
     <div id="contentField">
       <v-row id="rowField">
-        <v-col sm="3" cols="12">
+        <v-col sm="6" cols="12">
           <v-text-field v-model="code" label="Code Number" id="inputCode"></v-text-field>
             <!-- 여기에 vuex에 Guest아이디로 넘겨주는 기능 추가하면됨 -->
             <v-btn
-              class="ma-2"
+              class="ma-2 btnHome"
               style="width:200px"
               outlined
               color="indigo"
@@ -24,13 +24,13 @@
             </v-btn>
 
           <router-link :to="'/channel/create'">
-            <v-btn class="ma-2" style="width:200px" outlined color="indigo" @click="create()">Create</v-btn>
+            <v-btn class="ma-2 btnHome" style="width:200px" outlined color="indigo" @click="create()">Create</v-btn>
           </router-link>
-          <v-btn class="ma-2" style="width:200px" outlined color="red" @click="loginWithGoogle">
+          <v-btn class="ma-2 btnHome" style="width:200px" outlined color="red" @click="loginWithGoogle">
             <v-icon color="red">fa-google</v-icon>&nbsp;Login with Google
           </v-btn>
 
-          <v-btn class="ma-2" style="width:200px" outlined color="red" @click="logout">
+          <v-btn class="ma-2 btnHome" style="width:200px" outlined color="red" @click="logout">
             <v-icon color="red">fa-google</v-icon>&nbsp;Logout
           </v-btn>
         </v-col>
@@ -83,9 +83,9 @@ export default {
 <style>
 #rowField {
   margin-left: 8%;
-  margin-top: 0px;
-  position:absolute;
-  bottom:100px;
+  margin-top: 70%;
+  position:fixed;
+  bottom:10%;
 }
 
 #imgBanner {
@@ -96,14 +96,18 @@ export default {
 }
 
 #contentField {
-  position: absolute;
+  min-width: 400px;
+  min-height: 800px;
+  position: fixed;
   height: 100%;
   width: 100%;
   z-index: 5;
 }
 
 #containerHome {
-  /* position: fixed; */
+  position: fixed;
+  min-width: 500px;
+  min-height: 500px;
   height: 100vh;
   width: 100vw;
 }
@@ -114,4 +118,15 @@ export default {
   margin-left:10%;
   font-size: 1em;
 }
+
+.btnHome {
+  font-size: 0.8em !important;
+}
+
+@media (max-height: 500px) {
+  #bannerText {
+    display: none;
+  }
+}
+
 </style>
