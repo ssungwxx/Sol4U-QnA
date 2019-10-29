0e73c49fb3414051a46424007c9651ba8658ccb8
limseongwoo <joytjddn@naver.com>
2019-10-29 15:44:34 +0900

    Update HeaderWeb.vue
    ssafy go dashboard

--- ssafy-qna/src/components/HeaderWeb.vue
+++ ssafy-qna/src/components/HeaderWeb.vue
@@ -3,7 +3,7 @@
         <div style="display:inline-block; margin-top:2vw; margin-bottom:10vw;">
           <v-img src = "../assets/Icon.png" style="height:5vw; width:5vw;" />
         </div>
-        <router-link to="/" class="RouterLink">
+        <router-link to="/dashboard" class="RouterLink">
           <span class="code_banner">SSAFY</span>
         </router-link>
         <v-layout>
