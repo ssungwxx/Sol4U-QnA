b386f38ef7e62a3b114ffbdfd16008b4e3243912
김성우 <rlatjddn23@gmail.com>
2019-10-29 10:41:14 +0900

    Add snapshot Listner For reamtime

--- ssafy-qna/src/components/QnACard.vue
+++ ssafy-qna/src/components/QnACard.vue
@@ -1,65 +1,71 @@
 <template>
-  <v-flex xs12 round>
-    <v-card class="cardChild">
-      <v-card-text>
-        <div>
-          <p class="textQnA">{{getCard[cardId].question}}</p>
-        </div>
-        <p class="writeTimeText">
-          <v-icon small>access_time</v-icon>
-          &nbsp;
-          {{getCard[cardId].created_at.string}}
-          <v-btn icon small id="remove" @click="removeQ(getCard[cardId])">
-            <v-icon small color="red">delete_forever</v-icon>
-          </v-btn>
-        </p>
-        <div id="QnACardLike">
-          <!-- 하트 같은 아이콘으로 좋아요 개수 표시 -->
-          <v-card-actions>
-            <div class="QnACardLikeAction">
-              <v-btn
-                v-if="!likeBool"
-                @click="likeCheck(getCard[cardId].hitCount)"
-                text
-                icon
-                color="#00000033"
-              >
-                <v-icon>thumb_up_alt</v-icon>
-              </v-btn>
-              <v-btn v-else @click="likeCheck(getCard[cardId].hitCount)" text icon color="#ff0000">
-                <v-icon>thumb_up_alt</v-icon>
-              </v-btn>
-              <!-- 하트 개수 표시 영역 -->
-              <template v-if="likeCount(getCard[cardId].hitCount)">
-                <v-icon color="#cd7f32" id="likeIcon">thumb_up_alt</v-icon>
-                <!-- 하트 숫자 표시 -->
-                <span id="likeCount">...{{getCard[cardId].hitCount}}</span>
-              </template>
-            </div>
-            <v-btn @click="replyOn()" text color="deep-purple accent-4" id="replyBtn">
-              <v-icon large>reply</v-icon>
-            </v-btn>
-          </v-card-actions>
-        </div>
-        <div v-if="replyBool" pa-5 id="replyTextBox">
-          <v-textarea outlined label="답변을 등록하세요." id="replyText" v-model="replyText"></v-textarea>
+    <v-flex xs12 round>
+        <v-card class="cardChild">
+            <v-card-text>
+                <div>
+                    <p class="textQnA">{{getCard[cardId].question}}</p>
+                </div>
+                <p class="writeTimeText">
+                    <v-icon small>access_time</v-icon>
+                    &nbsp;
+                    {{getCard[cardId].created_at.string}}
+                    <v-btn icon small id="remove" @click="removeQ(getCard[cardId])">
+                        <v-icon small color="red">delete_forever</v-icon>
+                    </v-btn>
+                </p>
+                <div id="QnACardLike">
+                    <!-- 하트 같은 아이콘으로 좋아요 개수 표시 -->
+                    <v-card-actions>
+                        <div class="QnACardLikeAction">
+                            <v-btn
+                                v-if="!likeBool"
+                                @click="likeCheck(getCard[cardId].hitCount)"
+                                text
+                                icon
+                                color="#00000033"
+                            >
+                                <v-icon>thumb_up_alt</v-icon>
+                            </v-btn>
+                            <v-btn
+                                v-else
+                                @click="likeCheck(getCard[cardId].hitCount)"
+                                text
+                                icon
+                                color="#ff0000"
+                            >
+                                <v-icon>thumb_up_alt</v-icon>
+                            </v-btn>
+                            <!-- 하트 개수 표시 영역 -->
+                            <template v-if="likeCount(getCard[cardId].hitCount)">
+                                <v-icon color="#cd7f32" id="likeIcon">thumb_up_alt</v-icon>
+                                <!-- 하트 숫자 표시 -->
+                                <span id="likeCount">...{{getCard[cardId].hitCount}}</span>
+                            </template>
+                        </div>
+                        <v-btn @click="replyOn()" text color="deep-purple accent-4" id="replyBtn">
+                            <v-icon large>reply</v-icon>
+                        </v-btn>
+                    </v-card-actions>
+                </div>
+                <div v-if="replyBool" pa-5 id="replyTextBox">
+                    <v-textarea outlined label="답변을 등록하세요." id="replyText" v-model="replyText"></v-textarea>
 
-          <v-btn color="success" id="btnReply" @click="submitButton()">SUBMIT</v-btn>
-          <v-spacer style="clear: both;"></v-spacer>
-        </div>
-        <div id="QnACardReply" v-for="i in replyCnt" :key="i">
-          <!-- 답변에 대한 공간 -->
-          <div class="textReply">
-            <slot name="qnaReply"></slot>
-          </div>
-          <p class="writeTimeText">
-            <v-icon small>access_time</v-icon>&nbsp;
-            <slot name="qnaReplyTime"></slot>
-          </p>
-        </div>
-      </v-card-text>
-    </v-card>
-  </v-flex>
+                    <v-btn color="success" id="btnReply" @click="submitButton()">SUBMIT</v-btn>
+                    <v-spacer style="clear: both;"></v-spacer>
+                </div>
+                <div id="QnACardReply" v-for="i in replyCnt" :key="i">
+                    <!-- 답변에 대한 공간 -->
+                    <div class="textReply">
+                        <slot name="qnaReply"></slot>
+                    </div>
+                    <p class="writeTimeText">
+                        <v-icon small>access_time</v-icon>&nbsp;
+                        <slot name="qnaReplyTime"></slot>
+                    </p>
+                </div>
+            </v-card-text>
+        </v-card>
+    </v-flex>
 </template>
 
 <script>
@@ -67,78 +73,104 @@ import FirebaseService from "../services/FirebaseService";
 import QnAPage from "../views/QnAPage";
 
 export default {
-  name: "QnACard",
-  props: {
-    cardId: { type: Number },
-    docId: { type: String }
-  },
-  data: () => ({
-    // like or not check boolean var
-    likeBool: false,
-    replyCnt: 0,
-    replyBool: false
-  }),
-  computed: {
-    getCard() {
-      var list = this.$store.state.cardList;
-      return this.$store.state.cardList;
-    }
-  },
-  mounted() {},
-  methods: {
-    likeCheck(num) {
-      if (this.likeBool) {
-        this.likeBool = false;
-        // console.log(this.getCard[this.cardId].questionDocId);
-        FirebaseService.questionHit(
-          this.docId,
-          this.getCard[this.cardId].questionDocId,
-          -1
-        );
-        // this.likeCnt = this.likeCnt == 0 ? 0 : this.likeCnt - 1;
-      } else {
-        this.likeBool = true;
-        FirebaseService.questionHit(
-          this.docId,
-          this.getCard[this.cardId].questionDocId,
-          1
-        );
-      }
-      if (num >= 1 && num < 7) {
-        document.getElementById("likeIcon").style.color = "#cd7f32";
-        document.getElementById("likeCount").style.fontSize = "0.9em";
-      } else if (num >= 7 && num < 15) {
-        document.getElementById("likeIcon").style.color = "#c0c0c0";
-        document.getElementById("likeCount").style.fontSize = "1.0em";
-      } else if (num >= 15) {
-        document.getElementById("likeIcon").style.color = "#ffd700";
-        document.getElementById("likeCount").style.fontSize = "1.2em";
-      }
+    name: "QnACard",
+    props: {
+        cardId: { type: Number },
+        docId: { type: String }
     },
-    likeCount(num) {
-      if (num > 0) {
-        return true;
-      } else return false;
+    data: () => ({
+        // like or not check boolean var
+        likeBool: false,
+        replyCnt: 0,
+        replyBool: false
+    }),
+    computed: {
+        getCard() {
+            var list = this.$store.state.cardList;
+            return this.$store.state.cardList;
+        }
     },
-    replyOn() {
-      if (this.replyBool) {
-        this.replyBool = false;
-      } else {
-        this.replyBool = true;
-      }
+    mounted() {},
+    methods: {
+        likeCheck(num) {
+            if (this.likeBool) {
+                this.likeBool = false;
+                // console.log(this.getCard[this.cardId].questionDocId);
+                FirebaseService.questionHit(
+                    this.docId,
+                    this.getCard[this.cardId].questionDocId,
+                    -1
+                );
+                // this.likeCnt = this.likeCnt == 0 ? 0 : this.likeCnt - 1;
+            } else {
+                this.likeBool = true;
+                FirebaseService.questionHit(
+                    this.docId,
+                    this.getCard[this.cardId].questionDocId,
+                    1
+                );
+            }
+            if (num >= 1 && num < 7) {
+                document.getElementById("likeIcon").style.color = "#cd7f32";
+                document.getElementById("likeCount").style.fontSize = "0.9em";
+            } else if (num >= 7 && num < 15) {
+                document.getElementById("likeIcon").style.color = "#c0c0c0";
+                document.getElementById("likeCount").style.fontSize = "1.0em";
+            } else if (num >= 15) {
+                document.getElementById("likeIcon").style.color = "#ffd700";
+                document.getElementById("likeCount").style.fontSize = "1.2em";
+            }
+        },
+        likeCount(num) {
+            if (num > 0) {
+                return true;
+            } else return false;
+        },
+        replyOn() {
+            if (this.replyBool) {
+                this.replyBool = false;
+            } else {
+                this.replyBool = true;
+            }
+        },
+        async removeQ(id) {
+            await FirebaseService.deleteQuestion(this.docId, id.questionDocId);
+            await QnAPage.getQuestions;
+        }
     },
-    async removeQ(id) {
-      await FirebaseService.deleteQuestion(this.docId, id.questionDocId);
-      await QnAPage.getQuestions;
-    }
-  },
-  created() {
-    const channelDoc = FirebaseService.firestore.collection("QnAChannels");
+    created() {
+        const channelDoc = FirebaseService.firestore
+            .collection("QnAChannels")
+            .doc("YBrlA3mK73iZyUHXqQb3")
+            .collection("Questions");
 
-    channelDoc.get().then(doc => {
-      doc.forEach(snapshots => {});
-    });
-  }
+        channelDoc.onSnapshot(snapshots => {
+            snapshots.docChanges().forEach(change => {
+                const data = {
+                    questioner: change.doc.data().questioner,
+                    question: change.doc.data().question,
+                    created_at: change.doc.data().created_at,
+                    hitCount: change.doc.data().hitCount,
+                    hitList: change.doc.data().hitCount
+                };
+
+                if (change.type === "added") {
+                    console.log("New post: ", change.doc.data());
+                }
+                if (change.type === "modified") {
+                    console.log("Modified post: ", change.doc.data());
+                    console.log(this);
+                }
+                if (change.type === "removed") {
+                    console.log("Removed post: ", change.doc.data());
+                }
+            });
+        });
+
+        channelDoc.get().then(doc => {
+            doc.forEach(snapshots => {});
+        });
+    }
 };
 </script>
 
@@ -147,56 +179,56 @@ export default {
 }
 
 #btnReply {
-  float: right;
-  font-family: "Lexend Deca", sans-serif;
+    float: right;
+    font-family: "Lexend Deca", sans-serif;
 }
 
 #replyTextBox {
-  padding: 1% 4%;
-  width: 100%;
+    padding: 1% 4%;
+    width: 100%;
 }
 
 #QnACardReply {
-  margin-left: 2%;
+    margin-left: 2%;
 }
 
 #QnACardLike {
-  margin-bottom: 1%;
+    margin-bottom: 1%;
 }
 
 .QnACardLikeAction {
-  width: 100%;
-  text-align: left;
+    width: 100%;
+    text-align: left;
 }
 
 #likeCount {
-  font-family: "Lexend Deca", sans-serif;
-  margin-left: -5px;
-  color: grey;
-  font-size: 0.8em;
-  vertical-align: bottom;
+    font-family: "Lexend Deca", sans-serif;
+    margin-left: -5px;
+    color: grey;
+    font-size: 0.8em;
+    vertical-align: bottom;
 }
 
 .writeTimeText {
-  padding: 0% 0% 0% 1%;
-  margin-bottom: 0px !important;
-  font-family: "Lexend Deca", sans-serif;
-  justify-content: center;
-  font-size: 0.9em;
+    padding: 0% 0% 0% 1%;
+    margin-bottom: 0px !important;
+    font-family: "Lexend Deca", sans-serif;
+    justify-content: center;
+    font-size: 0.9em;
 }
 
 .textQnA {
-  padding: 3% 0% 0% 1%;
-  font-family: "Lexend Deca", sans-serif;
-  font-size: 1.5em;
-  color: midnightblue;
+    padding: 3% 0% 0% 1%;
+    font-family: "Lexend Deca", sans-serif;
+    font-size: 1.5em;
+    color: midnightblue;
 }
 
 .textReply {
-  font-family: "Lexend Deca", sans-serif;
-  font-size: 1.3em;
-  margin-bottom: 1%;
-  color: cadetblue;
+    font-family: "Lexend Deca", sans-serif;
+    font-size: 1.3em;
+    margin-bottom: 1%;
+    color: cadetblue;
 }
 
 #replyBtn {
