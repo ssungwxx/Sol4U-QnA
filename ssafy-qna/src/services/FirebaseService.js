b69f5dd2469a46beab8a6f308bfba19ffa2a6b12
ykk2006 <ykk2006@naver.com>
2019-11-25 21:06:19 +0900

    Update FirebaseService.js

--- ssafy-qna/src/services/FirebaseService.js
+++ ssafy-qna/src/services/FirebaseService.js
@@ -2,18 +2,6 @@ import firebase from "firebase";
 import "firebase/firestore";
 import "firebase/auth";
 
-// Setup Firebase
-const config = {
-  apiKey: "AIzaSyCRq_nISqiR-itSoaVPS8KyrxiEdalKddo",
-  authDomain: "ssafyfinal.firebaseapp.com",
-  databaseURL: "https://ssafyfinal.firebaseio.com",
-  projectId: "ssafyfinal",
-  storageBucket: "ssafyfinal.appspot.com",
-  messagingSenderId: "752151326570",
-  appId: "1:752151326570:web:d4d773f1ea0237a4273f13",
-  measurementId: "G-ZE1GDYK77V"
-};
-
 firebase.initializeApp(config);
 
 const firestore = firebase.firestore();
